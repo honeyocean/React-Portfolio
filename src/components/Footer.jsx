f9567ad3034e0661576329fc662b836d6521d348
Victoria Cerda <125603738+honeyocean@users.noreply.github.com>
2025-03-12 22:13:16 -0500

    Add files via upload

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import "../css/footer.css";
+
+export default function Footer(){
+    return(
+        <footer>
+            <a href = "https://github.com/honeyocean">
+            <img src = "../src/assets/github-pic.png" alt = "GitHub Account"></img>
+            </a>
+        </footer>
+    )
+}
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import "../css/header.css";
+import NavTabs from "./NavTabs";
+
+function Header(){
+    return (
+    <header>
+        <h1>Victoria Cerda</h1>
+        <NavTabs/>
+    </header>
+    )
+}
